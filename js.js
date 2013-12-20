bf4564c9ec51dd6e17cf2aba2ca2ef1d23d41e9c
useff <useffc@gmail.com>
2013-12-20 16:53:18 -0500

    first gh-pages commit

---
+++ js.js
@@ -0,0 +1,16 @@
+/*needs to make each "cell" iterate through HSL colors  */
+
+function randomColor() {
+	var newHue = Math.floor(Math.random() * 361);
+	var newSat = Math.floor(Math.random() * 101);
+	var newLight = Math.floor(Math.random() * 101);
+	var newAlpha = Math.random();
+	return "hsla("+ newHue + ", " + newSat + "%, " + newLight + "%, "  +"1)";
+}
+
+
+setInterval (function() {
+$('.matrix li').each(function() {
+	$(this).css("background-color", randomColor)});
+}, 350);
+
