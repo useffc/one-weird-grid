105dc983ac69672461f485ad3810069b493f3fe4
useff <useffc@gmail.com>
2013-12-19 23:21:13 -0500

    initial commit

---
+++ js.js
@@ -0,0 +1,13 @@
+/*needs to make each "cell" iterate through HSL colors  */
+
+function randomColor() {
+	var newHue = Math.floor(Math.random() * 361);
+	var newSat = Math.floor(Math.random() * 101);
+	var newLight = Math.floor(Math.random() * 101);
+	var newAlpha = Math.random();
+	return "hsla("+ newHue + ", " + newSat + "%, " + newLight + "%, "  +"1)";
+}
+
+
+setInterval (function() {
+$('.matrix li').css("background-color", randomColor())}, 500);
